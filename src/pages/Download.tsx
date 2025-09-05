054265660d4ab4783af95866980919e9d7001522
eljoker22 <eljokermano50@gmail.com>
2025-09-05 21:52:49 +0200

    add download

--- src/pages/Download.tsx
+++ src/pages/Download.tsx
@@ -18,7 +18,7 @@ function DownloadPage() {
   ];
 
   const devices = [
-    { icon: Monitor, name: 'Windows', url: 'AutomateDms.zip' },
+    { icon: Monitor, name: 'Windows', url: '/AutomateDms.zip' },
     { icon: Apple, name: 'macOS', url: '' }
   ];
 
@@ -97,7 +97,7 @@ function DownloadPage() {
                 {/* Download Button */}
                 {devices.map((dev) =>
                 dev.name === selectedOS && (
-                    <a key={dev.name} href={`files/${dev.url}`} download={dev.url}>
+                    <a key={dev.name} href={dev.url} download>
                     <button className="bg-gradient-to-r from-[#5B51D8] to-[#7C3AED] text-white px-8 py-3 rounded-lg font-semibold hover:from-[#6B61E8] hover:to-[#8B4AF8] transition-all duration-200 transform hover:scale-105 shadow-lg flex items-center space-x-2">
                         <Download className="w-5 h-5" />
                         <span>Download</span>
